d5fd79124816e6c9f48af823142f4d6c199b3cf4
Oleksii Sodolinskyi <sodolinskyi@gmail.com>
2020-06-01 09:49:29 +0200

    01 done 02 done

--- src/01_odd_integers/index.js
+++ src/01_odd_integers/index.js
@@ -2,6 +2,11 @@
 //
 // For example if our array is 1,3,5,6,8,2,0,11 the result will be 1,3,5,11
 
-exports.oddIntegers = function(data) {
-    //TODO implement me
-}
+exports.oddIntegers = function (data) {
+	//TODO implement me
+	const arr = [1, 3, 5, 6, 8, 2, 0, 11];
+
+	let odds = arr.filter((n) => n % 2);
+
+	return odds;
+};
--- src/02_min_max/index.js
+++ src/02_min_max/index.js
@@ -1,10 +1,24 @@
 // Find the biggest and the smallest number in array of integers.
 //
 // RESTRICTIONS: do this without using sort, min or max functionalities provided by language.
-// 
+//
 // For example if we have an array 10,4,5,7,99,1 the smallest number will be 1 and the biggest would be 99.
 // Return it as a JSON object with properties min and max.
 
-exports.minMax = function(data) {
-    //TODO implement me
-}
+exports.minMax = function (data) {
+	//TODO implement me
+	const arr = [10, 4, 5, 7, 99, 1];
+
+	var lg = arr[0];
+	var sm = arr[0];
+
+	for (var i = 1; i < arr.length; i++) {
+		if (arr[i] > lg) {
+			lg = arr[i];
+		} else if (arr[i] < sm) {
+			sm = arr[i];
+		}
+	}
+
+	return [{ min: sm, max: lg }];
+};
